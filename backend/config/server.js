53f5c8915a5b3633c18a576d318e21449a89662c
sarocha-chang <sarocha_chang@cmu.ac.th>
2021-09-21 16:27:07 +0700

    Finish

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9ef5f2d40307fcb77878c04bccc901d2'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c16b0a0d-d686-4ebe-873c-44fd8030bb3a'
+};
